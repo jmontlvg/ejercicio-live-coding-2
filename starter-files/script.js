4fb592858cf3d93e5ab44b6d140680eedc4a84ec
Jackeline Montalvo <jackeline.montalvog@gmail.com>
2025-01-24 12:31:11 -0600

    Se agrega primer avance

--- starter-files/script.js
+++ starter-files/script.js
@@ -11,3 +11,18 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+
+//Step 1. Create a fetchAnswer function and call the API
+async function fetchAnswer() {
+    try {
+      //Primero llamo a la API
+      const response = await fetch(API_ENDPOINT);
+  
+      //Posteriorment con 'response.ok' verifico si la respuesta fue exitosa
+      if (!response.ok) {
+        
+      }
+    }
+}
+
+addEventListener()=>
